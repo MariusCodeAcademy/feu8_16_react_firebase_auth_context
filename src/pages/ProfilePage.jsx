89e150d3e6c6cfdf3905377a8c61bba4f967d47f
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-28 12:05:49 +0300

    profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,8 +1,23 @@
 export default function ProfilePage() {
+  // 1. updateProfile funkcijos pagalba atnaujinti reiksmes
+  // https://firebase.google.com/docs/auth/web/manage-users#update_a_users_profile
+
+  // pasiimti displayName protoUrl info is konteksto
+
+  // pateikiant forma paimti reiksmes is displayName ir protoUrl
+
   return (
     <div className='container'>
       <h1>ProfilePage</h1>
+      <h2>User display name</h2>
+      <img src='#' alt='Profile image' />
       <p>Welcome to Your own space</p>
+
+      <p>Turi buti forma, 2 inputai</p>
+      <p>displayName</p>
+      <p>protoUrl</p>
+
+      <p>jei pavyksta pridet email atnaujinima</p>
     </div>
   );
 }
