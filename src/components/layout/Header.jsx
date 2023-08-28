1568e0b8912869649c49a4478eaa2b8d90e77c7d
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-28 11:38:10 +0300

    login logout works

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,24 @@
 import { Link, NavLink } from 'react-router-dom';
 import './Header.scss';
 import { useAuth } from '../../store/AuthProvider';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../firebase/firebase';
 
 export default function Header() {
   const ctx = useAuth();
   console.log('ctx ===', ctx);
 
-  const isLoggedIn = false;
+  const isLoggedIn = ctx.isLoggedIn;
+
+  function logout() {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  }
 
   return (
     <header className='header'>
@@ -29,7 +41,7 @@ export default function Header() {
             </NavLink>
           )}
           {isLoggedIn && (
-            <NavLink className={'navLink'} to={'/login'}>
+            <NavLink onClick={logout} className={'navLink'} to={'/login'}>
               Logout
             </NavLink>
           )}
