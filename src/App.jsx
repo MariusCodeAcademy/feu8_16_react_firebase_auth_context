363e3fb79d8e514bb3a4b812a24d6a2676f8984a
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-28 11:57:11 +0300

    protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { app } from './firebase/firebase';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import Header from './components/layout/Header';
 import ProfilePage from './pages/ProfilePage';
+import { useAuth } from './store/AuthProvider';
 
 export default function App() {
-  console.log('app', app);
+  const ctx = useAuth();
   return (
     <div className=''>
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/profile' element={<ProfilePage />} />
+        {ctx.isLoggedIn && <Route path='/profile' element={<ProfilePage />} />}
+        <Route
+          path='/login'
+          element={
+            ctx.isLoggedIn ? <Navigate to={'/profile'} /> : <LoginPage />
+          }
+        />
       </Routes>
     </div>
   );
