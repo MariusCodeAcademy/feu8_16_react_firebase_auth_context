bc7ef13aafdd701031b38acf9650e810e408b72b
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-31 11:35:53 +0300

    delete works

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,35 +1,40 @@
-import { addDoc, collection, getDocs } from 'firebase/firestore';
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+} from 'firebase/firestore';
 import { db } from '../firebase/firebase';
 import { useEffect, useState } from 'react';
 
 export default function TodoPage() {
   const [booksArr, setBooksArr] = useState([]);
 
+  async function getBooksFb() {
+    // console.log('lets get some books');
+
+    const querySnapshot = await getDocs(collection(db, 'books'));
+    // console.log('querySnapshot ===', querySnapshot);
+    const dataBack = [];
+    querySnapshot.forEach((doc) => {
+      const singleBookObj = {
+        id: doc.id,
+        ...doc.data(),
+      };
+      // singleBookObj.id = doc.id;
+      // console.log('singleBookObj ===', singleBookObj);
+      dataBack.push(singleBookObj);
+    });
+    // console.log('dataBack ===', dataBack);
+    setBooksArr(dataBack);
+  }
   useEffect(() => {
-    async function getBooksFb() {
-      // console.log('lets get some books');
-
-      const querySnapshot = await getDocs(collection(db, 'books'));
-      // console.log('querySnapshot ===', querySnapshot);
-      const dataBack = [];
-      querySnapshot.forEach((doc) => {
-        const singleBookObj = {
-          id: doc.id,
-          ...doc.data(),
-        };
-        // singleBookObj.id = doc.id;
-        // console.log('singleBookObj ===', singleBookObj);
-        dataBack.push(singleBookObj);
-      });
-      // console.log('dataBack ===', dataBack);
-      setBooksArr(dataBack);
-    }
-
     getBooksFb();
   }, []);
 
   const newBook = {
-    title: 'To Kill a Mockingbird',
+    title: 'To Kill a Dublicate',
     year: 1960,
     author: 'Harper Lee',
     isOnSale: false,
@@ -40,12 +45,24 @@ export default function TodoPage() {
     try {
       const docRef = await addDoc(collection(db, 'books'), newBook);
       console.log('Document written with ID: ', docRef.id);
+      getBooksFb();
     } catch (e) {
       console.error('Error adding document: ', e);
     }
   }
-
-  // await deleteDoc(doc(db, "kolekcija", "dokumento id"));
+  async function handleDelete(idToDelete) {
+    console.log('idToDelete ===', idToDelete);
+    // await deleteDoc(doc(db, "kolekcija", "dokumento id"));
+    try {
+      const rez = await deleteDoc(doc(db, 'books', idToDelete));
+      // sekme
+      console.log('pavyko istrinti', rez);
+      getBooksFb();
+    } catch (error) {
+      // nesekme
+      console.warn('handleDelete error ===', error);
+    }
+  }
   // atspausdinti console, ar pavyko ar ne
   return (
     <div className='container'>
@@ -59,8 +76,8 @@ export default function TodoPage() {
       <ul>
         {booksArr.map((bookObj) => (
           <li key={bookObj.id}>
-            title: {bookObj.title}, author: {bookObj.author}
-            <button>Delete</button>
+            title: {bookObj.title}, author: {bookObj.author}{' '}
+            <button onClick={() => handleDelete(bookObj.id)}>Delete</button>
           </li>
         ))}
       </ul>
