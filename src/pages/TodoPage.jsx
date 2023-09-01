9b93808cd1c4fe4d5ac29d7ad1309136359498c8
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-09-01 10:02:39 +0300

    todo print

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,5 +1,6 @@
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
 import { db } from '../firebase/firebase';
+import { useEffect, useState } from 'react';
 
 const initTodos = [
   { title: 'Buy Eggs', done: false, date: '' },
@@ -8,6 +9,8 @@ const initTodos = [
 ];
 
 export default function TodoPage() {
+  const [localTodoArr, setLocalTodoArr] = useState([]);
+  console.log('localTodoArr ===', localTodoArr);
   async function initTodo() {
     console.log('initTodo');
 
@@ -19,16 +22,44 @@ export default function TodoPage() {
     }
   }
 
-  function getTodosFromFireStore() {
+  async function getTodosFromFireStore() {
     // parsiusti ir iskonsolinti todos masyva su id
+
+    try {
+      const querySnapshot = await getDocs(collection(db, 'todos'));
+      console.log('querySnapshot ===', querySnapshot);
+      // sekme
+      console.log('success');
+      const todosBack = [];
+      querySnapshot.forEach((doc) => {
+        // console.log(`${doc.id} =>`, doc.data());
+        todosBack.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      console.log('todosBack ===', todosBack);
+      setLocalTodoArr(todosBack);
+    } catch (error) {
+      console.warn('error geting todos', error);
+    }
   }
+
+  useEffect(() => {
+    getTodosFromFireStore();
+  }, []);
 
   return (
     <div className='container'>
       <h1>TodoPage</h1>
       <p>Welcome to TodoPage page</p>
       {/* <button onClick={initTodo}>initTodo</button> */}
-      <button onClick={getTodosFromFireStore}>get Todos</button>
+      {/* <button onClick={getTodosFromFireStore}>get Todos</button> */}
+
+      <ul>
+        {/* atspausdinti visus todo */}
+        <li>todo title</li>
+      </ul>
     </div>
   );
 }
